35d976d0835f49f8c72da7cbbde9768bbd33f643
Vogelgesang <fvogelgesang@deloitte.de>
2020-03-22 11:24:15 +0100

    implement route for filtering of resources

--- localhorst/routes/elasticsearch.js
+++ localhorst/routes/elasticsearch.js
@@ -7,4 +7,14 @@ router.get('/get-all-resources', (req, res, next) => {
     res.render('index', { title: 'Elasticsearch Result' });
 });
 
+router.get('/filter-resources', (req, res) => {
+    let { category, industry, type, text } = req.body
+    res.send({
+        category,
+        industry,
+        type,
+        text
+    })
+});
+
 module.exports = router;
