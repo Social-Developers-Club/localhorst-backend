226a686a113f2bcac89e8948a9e6a1bee93712c9
philenius <philenius@users.noreply.github.com>
2020-03-22 11:42:52 +0100

    add HTTP endpoint for querying all recommendations

--- localhorst/routes/elasticsearch.js
+++ localhorst/routes/elasticsearch.js
@@ -3,8 +3,26 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/get-all-resources', (req, res, next) => {
-    res.render('index', { title: 'Elasticsearch Result' });
+const { Client } = require('@elastic/elasticsearch');
+const elasticsearchNode = process.env.ELASTICSEARCH_NODE || 'http://localhost:9200';
+const elasticsearchIndex = process.env.ELASTICSEARCH_INDEX || 'recommendations';
+
+router.get('/get-all-recommendations', (req, res, next) => {
+    const client = new Client({ node: elasticsearchNode });
+    client.search({
+        index: elasticsearchIndex,
+        body: {
+            query: {
+                match_all: {}
+            }
+        }
+    }).then((searchResponse) => {
+        const hits = searchResponse.body.hits.hits.map(hit => hit['_source']);
+        res.setHeader('Content-Type', 'application/json');
+        res.json(hits);
+    }).catch((err) => {
+        res.status(503).send('Failed to query Elasticsearch');
+    });
 });
 
 module.exports = router;
