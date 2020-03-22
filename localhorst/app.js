1eb29a1bf1ba6e925342ab63b4000311032c20a9
philenius <philenius@users.noreply.github.com>
2020-03-22 12:34:27 +0100

    change endpoint naming so that it conforms to REST

--- localhorst/app.js
+++ localhorst/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/es/', elasticsearchRouter);
+app.use('/recommendations/', elasticsearchRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- localhorst/routes/elasticsearch.js
+++ localhorst/routes/elasticsearch.js
@@ -7,60 +7,72 @@ const { Client } = require('@elastic/elasticsearch');
 const elasticsearchNode = process.env.ELASTICSEARCH_NODE || 'http://localhost:9200';
 const elasticsearchIndex = process.env.ELASTICSEARCH_INDEX || 'recommendations';
 
-router.get('/get-all-recommendations', (req, res, next) => {
+router.get('/', (req, res) => {
     const client = new Client({ node: elasticsearchNode });
-    client.search({
-        index: elasticsearchIndex,
-        body: {
-            query: {
-                match_all: {}
+
+    const category = req.query.category;
+    const industry = req.query.industry;
+    const type = req.query.type;
+    const text = req.query.text;
+
+    let searchQuery;
+
+    // match all documents when no query params given; else build complex query
+    if (!category && !industry && !type && !text) {
+        searchQuery = {
+            "query": {
+                "match_all": {}
             }
         }
-    }).then((searchResponse) => {
-        const hits = searchResponse.body.hits.hits.map(hit => hit['_source']);
-        res.setHeader('Content-Type', 'application/json');
-        res.json(hits);
-    }).catch((err) => {
-        res.status(503).send('Failed to query Elasticsearch');
-    });
-});
+    } else {
 
-router.get('/filter-resources', (req, res) => {
-    const client = new Client({ node: elasticsearchNode });
-    const { category, industry, type, text } = req.body;
+        searchQuery = {
+            "query": {
+                "bool": {
+                    "must": []
+                }
+            }
+        };
 
-    let searchQuery = {
-        "query": {
-            "bool": {
-                "must": []
-            }}};
+        if (category) {
+            searchQuery.query.bool.must.push({
+                "term": {
+                    "category": {
+                        "value": category
+                    }
+                }
+            })
+        }
 
-    if (category) {
-        searchQuery.query.bool.must.push({
-            "term": {
-                "category": {
-                    "value": category
-                }}})}
+        if (industry) {
+            searchQuery.query.bool.must.push({
+                "term": {
+                    "industry": {
+                        "value": industry
+                    }
+                }
+            })
+        }
 
-    if (industry) {
-        searchQuery.query.bool.must.push({
-            "term": {
-                "industry": {
-                    "value": industry
-                }}})}
+        if (type) {
+            searchQuery.query.bool.must.push({
+                "term": {
+                    "type": {
+                        "value": type
+                    }
+                }
+            })
+        }
 
-    if (type) {
-        searchQuery.query.bool.must.push({
-            "term": {
-                "type": {
-                    "value": type
-                }}})}
+        if (text) {
+            searchQuery.query.bool.must.push({
+                "match": {
+                    "recommendation": text
+                }
+            })
+        }
 
-    if (text) {
-        searchQuery.query.bool.must.push({
-            "match": {
-                "recommendation": text
-            }})}
+    }
 
     client.search({ index: elasticsearchIndex, body: searchQuery })
         .then(searchResults => {
@@ -68,7 +80,7 @@ router.get('/filter-resources', (req, res) => {
                 return {
                     id: hit['_id'],
                     score: hit['_score'],
-                    values: hit['_source']
+                    doc: hit['_source']
                 }
             });
             res.setHeader('Content-Type', 'application/json');
